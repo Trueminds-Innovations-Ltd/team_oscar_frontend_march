2ae091b96a09ae6add3cb3b6824e447f282a1049
Bolazcoding <bolajiadelabu0@gmail.com>
2026-04-10 15:25:45 +0100

    loading spinner and changes made

--- Frontend/src/features/dashboard/components/DashboardMetrics.jsx
+++
@@ -1,9 +0,0 @@
-function DashboardMetrics ({children}){
-  return (
-    <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
-        {children}
-    </div>
-  )
-}
-
-export default DashboardMetrics;
